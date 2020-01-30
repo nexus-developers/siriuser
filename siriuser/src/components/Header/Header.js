e4f2c58a1258850491588ec1bfe4401fb5d2ea95
Geraldo Domingos <geraldo.dsn2000@gmail.com>
2020-01-30 11:51:46 -0300

    sidebar and routes

--- siriuser/src/components/Header/Header.js
+++ siriuser/src/components/Header/Header.js
@@ -11,9 +11,8 @@ export default function Header() {
             <Link to='/' className="navbar-brand ml-2" style={{fontWeight: 'bold'}}>
                 <img src={Logo} style={{width: '200px'}} alt="logo"/>
             </Link>
-                <input className="form-control form-control-light w-100 ml-5 mr-4" type="search" placeholder="Search" aria-label="Search" style={{backgroundColor: '#06E500', border: 'none'}}/>
-              <ul className="navbar-nav px-3">
-                  <li className="nav-item text-nowrap">
+              <ul className="navbar-nav px-3 ml-auto">
+                  <li className="nav-item text-nowrap ">
                       <Link to='' className="nav-link active" style={{fontWeight: 'bold', color: '#5D5D5D'}}>Sign Out</Link>
                   </li>
               </ul>
--- siriuser/src/routes.js
+++ siriuser/src/routes.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Dashboard from  './pages/Dashboard'
+import Projects from './pages/Projects'
 
 export default function routes() {
   return (
     <Router>
         <Switch>
             <Route path='/' exact  component={Dashboard}/>
+            <Route path='/projects' component={Projects}/> 
         </Switch>
     </Router>
   );
