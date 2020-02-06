949dbb27a4cf0ae563bc470e8ad96054d90cb37d
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2020-02-06 10:09:13 -0300

    cadastro, delete e requisição

--- siriuser/src/pages/Contacts/Contacts.js
+++ siriuser/src/pages/Contacts/Contacts.js
@@ -5,29 +5,88 @@ import { SecondContainer ,Contact, AddContact, Form, UlClients } from "./styles.
 
 import { TiDeleteOutline } from "react-icons/ti";
 
+import firebase from '../../firebase';
+
 export default class Contacts extends Component {
-  state = {
-    nome: '',
-    cpf: '',
-    categoria: '',
-    telefone: '',
+  constructor(props){
+    super(props);
+
+    this.state = {
+      clients: []
+    }
   }
 
-  handleAddName = event => { 
-    this.setState({ 
-      nome: event.target.value 
-    })
+  //gravar firebase
+  writeUserData = () => {
+    firebase.database().ref('/').set(this.state);
+    console.log('salvou so pra saber no console');
   }
-  handleAddCpf = event => { this.setState({ cpf: event.target.value }) }
-  handleAddCategory = event => { 
-    this.setState({ 
-      categoria: event.target.value
-     }) 
-     console.log(this.state.categoria);
-   }
-  handleAddTel = event => { this.setState({ telefone: event.target.value }) }
+
+  //requisição firebase
+  getUserData = () => {
+    const ref = firebase.database().ref('/');
+    ref.on('value', snapshot => {
+      const state = snapshot.val();
+      this.setState(state);
+    });
+    console.log('dados recuperados');
+  }
+  
+  componentDidMount() {
+    this.getUserData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState !== this.state){
+      this.writeUserData();
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const uid = this.refs.uid.value;
+    const name = this.refs.name.value;
+    const cpf = this.refs.cpf.value;
+    const categoria = this.refs.categoria.value;
+    const telefone = this.refs.telefone.value;
+
+    if(uid && name && cpf && categoria && telefone){
+      const { clients } = this.state;
+      const clientIndex = clients.findIndex(data => {
+        return data.uid == uid;
+      });
+      clients[clientIndex].name = name;
+      clients[clientIndex].cpf = cpf;
+      clients[clientIndex].categoria = categoria;
+      clients[clientIndex].telefone = telefone;
+      this.setState({ clients });
+    }
+    else if(name && cpf && categoria && telefone){
+      const uid = new Date().getTime().toString();
+      const { clients } = this.state;
+      clients.push({ uid, name, cpf, categoria, telefone });
+      this.setState({ clients });
+    }
+
+    this.refs.uid.value = '';
+    this.refs.name.value = '';
+    this.refs.cpf.value = '';
+    this.refs.categoria.value = '';
+    this.refs.telefone.value = '';
+  }
+
+  //remover cliente
+  removeData = (client) => {
+    const { clients } = this.state;
+    const newState = clients.filter(data => {
+      return data.uid !== client.uid;
+    });
+    this.setState({ clients: newState });
+  }
 
   render() {
+    const { clients } = this.state;
     return(
       <Container>
         <SecondContainer className='row'>
@@ -35,26 +94,33 @@ export default class Contacts extends Component {
             <h1 className='text-center'>Seus clientes</h1>
             <p className='text-center'>Nós da Sirius, prezamos pelo sua interação com os clientes!</p>
             <UlClients>
-              <ul>
-                <button><TiDeleteOutline/></button>
-                <li>Geraldo Domingos</li>
-                <li>107.191.934-27</li>
-                <li>Empresarial</li>
-                <li>(81)99245-4451</li>
-              </ul>
+              {
+                clients.map(client => 
+                  <ul key={client.uid}>
+                    <button onClick={ () => this.removeData(client) }><TiDeleteOutline/></button>
+                    <li>{ client.name }</li>
+                    <li>{ client.cpf }</li>
+                    <li>{ client.categoria }</li>
+                    <li>{ client.telefone }</li>
+                  </ul>
+                  )
+              }
+              
             </UlClients>
           </Contact>
           <AddContact className='col-md-4'>
               <h1>Adicionar Clientes</h1>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
                 <div class="form-group">
+                  <input 
+                    type='hidden'
+                    ref='uid' />
                   <label>Nome:</label>
                   <input 
                     type="text" 
                     className="form-control"  
                     placeholder="Insira o nome do cliente"
-                    value={this.state.nome}
-                    onChange={this.handleAddName}
+                    ref='name'
                   />
                 </div>
                 <div class="form-group">
@@ -63,12 +129,11 @@ export default class Contacts extends Component {
                     type="text" 
                     className="form-control"  
                     placeholder="000.000.000-00"
-                    value={this.state.cpf}
-                    onChange={this.handleAddCpf}
+                    ref='cpf'
                   />
                 </div>
                 <label>Categoria:</label>
-                <select className="form-control" value={this.state.categoria} onChange={this.handleAddCategory}>
+                <select className="form-control" ref='categoria'>
                   <option value='Pessoal'>Cliente Empresarial</option>
                   <option value='Empresarial'>Cliente Pessoal</option>
                 </select>
@@ -78,12 +143,11 @@ export default class Contacts extends Component {
                     type="text" 
                     className="form-control"  
                     placeholder="(00) 98765-4321"
-                    value={this.state.telefone}
-                    onChange={this.handleAddTel}
+                    ref='telefone'
                   />
                 </div>
+                <button type='submit'>Cadastrar Cliente</button>
             </Form>
-              <button type='submit'>Cadastrar Cliente</button>
           </AddContact>
         </SecondContainer>
       </Container>
