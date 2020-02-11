98d0599981ef80dae4457bde717d94fde85c0090
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2020-02-11 07:18:32 -0300

    dados-dashboard

--- siriuser/src/pages/Dashboard/Dashboard.js
+++ siriuser/src/pages/Dashboard/Dashboard.js
@@ -1,132 +1,172 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Container } from '../../styles/Container'
 import { Cards, ProjectHistory, Card, CardBody, CardTitle, CardText, RowCards, CardFull } from './styles'
 
-export default function pages() {
-  return (
-    <Container className='row'>
-      <Cards className='col-md-5' >
-        <RowCards className='row'>
-          <Cards className='col-sm-6'>
-            <Card className='card shadow bg-light'>
-                <CardTitle className='card-header text-center'>
-                  Projetos Criados
-                </CardTitle>
-                <CardBody className='card-body'>
-                <CardText className='card-text text-center'>4 projetos criados</CardText>
-              </CardBody>
-            </Card>
-          </Cards>
+import firebase from '../../firebase';
 
-          <Cards className='col-sm-6'>
-            <Card className='card shadow bg-light'>
-                <CardTitle className='card-header text-center'>
-                  Valor Total das Propostas
-                </CardTitle>
-                <CardBody className='card-body'>
-                <CardText className='card-text text-center'>R$ 123.231.21</CardText>
-              </CardBody>
-            </Card>
-          </Cards>
+export default class pages extends Component {
+  state = {
+    projects: [],
+    clients: [],
+  }
+
+  //requisetar projetos
+  getUserData = () =>{
+    const ref = firebase.database().ref('/');
+    ref.on('value', snapshot => {
+      const state = snapshot.val();
+      this.setState(state);
+    });
+  }
+
+  componentDidMount(){
+    this.getUserData();
+  }
 
 
-        <Cards className='col-sm-12'>
-          <CardFull className='card shadow bg light'>
+  render() {
+    const { projects, clients } = this.state;
+
+    //contador proposta
+    let id = 0;
+    let aprovado = 0;
+    let rejeitado = 0;
+    projects.forEach(contador);
+    function contador(item){
+      const ref = firebase.database().ref('/projects/' + id);
+      ref.once('value', snapshot => {
+        const state = snapshot.val().etapa;
+        if(state == "Proposta Negociada"){
+          aprovado++;
+        }
+        else{
+          rejeitado++;
+        }
+      });
+      id++;
+    }
+
+    //contador cliente
+    let clientId = 0;
+    let pessoais = 0;
+    let empresariais = 0;
+    clients.forEach(clientes);
+    function clientes(item){
+      const ref = firebase.database().ref('/clients/' + clientId);
+      ref.once('value', snapshot => {
+        const state = snapshot.val().categoria;
+        if(state == "Pessoal"){
+          pessoais++;
+        }
+        else{
+          empresariais++;
+        }
+      });
+      clientId++;
+    }
+
+    return (
+      <Container className='row'>
+        <Cards className='col-md-5' >
+          <RowCards className='row'>
+            <Cards className='col-sm-6'>
+              <Card className='card shadow bg-light'>
                   <CardTitle className='card-header text-center'>
-                    Tipos de Clientes
+                    Projetos Criados
                   </CardTitle>
                   <CardBody className='card-body'>
-                    <ul>
-                      <li>
-                        <CardText className='card-text text-center'>Clientes Pessoais: 4 Clientes.</CardText>
-                      </li>
-                      <li>
-                      <CardText className='card-text text-center'>Clientes Empresariais: 4 Clientes.</CardText>
-                      </li>
-                    </ul>
-                  
-                  </CardBody>
-            </CardFull>
-          </Cards>
-
-
-          <Cards className='col-sm-6'>
-            <Card className='card shadow bg-success text-white'>
-                <CardTitle className='card-header text-center'>
-                  Projetos Aprovados
-                </CardTitle>
-                <CardBody className='card-body'>
-                <CardText className='card-text text-center'>8 Projetos Aprovados</CardText>
-              </CardBody>
-            </Card>
-          </Cards>
-
-          <Cards className='col-sm-6'>
-            <Card className='card shadow bg-danger text-white'>
-                <CardTitle className='card-header text-center'>
-                  Projetos Reprovados
-                </CardTitle>
-                <CardBody className='card-body'>
-                <CardText className='card-text text-center'>3 Projetos Rejeitados</CardText>
-              </CardBody>
-            </Card>
-          </Cards>
-        </RowCards>
-      </Cards>
-
-      
-      <ProjectHistory className='col-md-7'>
-        <RowCards>
-        <Cards className='col-sm-12'>
-          <CardFull className='card shadow bg light'>
+                  <CardText className='card-text text-center'>{projects.length} projetos criados</CardText>
+                </CardBody>
+              </Card>
+            </Cards>
+  
+            <Cards className='col-sm-6'>
+              <Card className='card shadow bg-light'>
                   <CardTitle className='card-header text-center'>
-                    Histórico de Projetos
+                    Valor Total das Propostas
                   </CardTitle>
                   <CardBody className='card-body'>
-                    <ul>
-                     <li>Geraldo Domingos</li>
-                     <li>Proposta Enviada</li>
-                     <li>800 kWh</li>
-                     <li>127 / 220</li>
-                     <li>Kit 4</li>
-                     <li>Bifásico</li>
-                    </ul>
-
-                    <ul>
-                     <li>Geraldo Domingos</li>
-                     <li>Proposta Enviada</li>
-                     <li>800 kWh</li>
-                     <li>127 / 220</li>
-                     <li>Kit 4</li>
-                     <li>Bifásico</li>
-                    </ul>
-
-                    <ul>
-                     <li>Geraldo Domingos</li>
-                     <li>Proposta Enviada</li>
-                     <li>800 kWh</li>
-                     <li>127 / 220</li>
-                     <li>Kit 4</li>
-                     <li>Bifásico</li>
-                    </ul>
-
-                    <ul>
-                     <li>Geraldo Domingos</li>
-                     <li>Proposta Enviada</li>
-                     <li>800 kWh</li>
-                     <li>127 / 220</li>
-                     <li>Kit 4</li>
-                     <li>Bifásico</li>
-                    </ul>
-                  
-                  </CardBody>
-            </CardFull>
-          </Cards>
-        </RowCards>
-      </ProjectHistory>
-    </Container>
-  );
+                  <CardText className='card-text text-center'>R$ 123.231.21</CardText>
+                </CardBody>
+              </Card>
+            </Cards>
+  
+  
+          <Cards className='col-sm-12'>
+            <CardFull className='card shadow bg light'>
+                    <CardTitle className='card-header text-center'>
+                      Tipos de Clientes
+                    </CardTitle>
+                    <CardBody className='card-body'>
+                      <ul>
+                        <li>
+                          <CardText className='card-text text-center'>Clientes Pessoais: { pessoais } Clientes.</CardText>
+                        </li>
+                        <li>
+                        <CardText className='card-text text-center'>Clientes Empresariais: { empresariais } Clientes.</CardText>
+                        </li>
+                      </ul>
+                    
+                    </CardBody>
+              </CardFull>
+            </Cards>
+  
+  
+            <Cards className='col-sm-6'>
+              <Card className='card shadow bg-success text-white'>
+                  <CardTitle className='card-header text-center'>
+                    Projetos Aprovados
+                  </CardTitle>
+                  <CardBody className='card-body'>
+                  <CardText className='card-text text-center'> { aprovado } Projetos Aprovados</CardText>
+                </CardBody>
+              </Card>
+            </Cards>
+  
+            <Cards className='col-sm-6'>
+              <Card className='card shadow bg-danger text-white'>
+                  <CardTitle className='card-header text-center'>
+                    Projetos Reprovados
+                  </CardTitle>
+                  <CardBody className='card-body'>
+                  <CardText className='card-text text-center'>{ rejeitado } Projetos Rejeitados</CardText>
+                </CardBody>
+              </Card>
+            </Cards>
+          </RowCards>
+        </Cards>
+  
+        
+        <ProjectHistory className='col-md-7'>
+          <RowCards>
+          <Cards className='col-sm-12'>
+            <CardFull className='card shadow bg light'>
+                    <CardTitle className='card-header text-center'>
+                      Histórico de Projetos
+                    </CardTitle>
+                    <CardBody className='card-body'>
+                      {
+                        projects.map(project =>
+                          <ul key={project.uid}>
+                            <li>{ project.client }</li>
+                            <li>{ project.etapa }</li>
+                            <li>{ project.consumo } kWh</li>
+                            <li>{ project.tensao }</li>
+                            <li>{ project.kit }</li>
+                            <li>{ project.fase }</li>
+                          </ul>
+                          )
+                      }
+                      
+                    </CardBody>
+              </CardFull>
+            </Cards>
+          </RowCards>
+        </ProjectHistory>
+      </Container>
+    );
+  }
 }
 
 
