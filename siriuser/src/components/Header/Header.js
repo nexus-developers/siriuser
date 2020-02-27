d258364bd4c1b8a2a9e3aa1501dcca60ba432f8e
Vickie <vcamilaxs@gmail.com>
2020-02-27 12:13:57 -0300

    modificações no header

--- siriuser/src/components/Header/Header.js
+++ siriuser/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Link } from 'react-router-dom'
  
@@ -6,39 +6,99 @@ import Logo from '../../styles/GlobalAssets/logo.png'
 
 import { FaRegUserCircle, FaBell, FaRegQuestionCircle, FaMedal } from 'react-icons/fa'
 
-import { Navbar } from './styles'
+import { Navbar, DropdownMenu } from './styles'
 
-export default function Header() {
-  return (
-      <div>
-        <Navbar className="navbar navbar-expand-lg navbar-light fixed-top flex-md-nowrap" style={{backgroundColor:'#fff' }}>
-            <Link to='/' className="navbar-brand ml-2" style={{fontWeight: 'bold'}}>
-                <img src={Logo} style={{width: '200px'}} alt="logo"/>
-            </Link>
-              <ul className="navbar-nav px-3 ml-auto">
-                  <li className="nav-item text-nowrap ">
-                      <Link to='' className="nav-link active" style={{fontWeight: 'bold', color: '#FFD000', marginRight: '40px'}}>
-                        <FaMedal size={28}/>
-                        <span style={{ color: '#000', marginRight: '10px' }}>INTEGRADOR OURO</span>
-                      </Link>
-                  </li>
-                  <li className="nav-item text-nowrap ">
-                      <Link to='' className="nav-link active" style={{fontWeight: 'bold', color: '#D3D3D3'}}>
-                        <FaRegQuestionCircle size={28}/>
-                      </Link>
-                  </li>
-                  <li className="nav-item text-nowrap ">
-                      <Link to='' className="nav-link active" style={{fontWeight: 'bold', color: '#D3D3D3'}}>
-                        <FaBell size={28}/>
-                      </Link>
-                  </li>
-                  <li className="nav-item text-nowrap ">
-                      <Link to='' className="nav-link active" style={{fontWeight: 'bold', color: '#00CA00'}}>
-                        <FaRegUserCircle size={28}/>
-                      </Link>
-                  </li>
-              </ul>
-        </Navbar> 
-    </div>
-  );
+export default class Header extends Component {
+  state = {
+    showMenu: false
+  }
+
+  showMenu = () =>{
+    
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+  
+  closeMenu = () =>{
+    this.setState({ showMenu: false }, () => {
+      document.removeEventListener('click', this.closeMenu);
+    });
+  }
+
+  render(){
+  const { showMenu } = this.state 
+
+    return (
+
+
+        <div>
+          <Navbar className="navbar navbar-expand-lg navbar-light fixed-top flex-md-nowrap" style={{backgroundColor:'#fff' }}>
+              <Link to='/' className="navbar-brand ml-2" style={{fontWeight: 'bold'}}>
+                  <img src={Logo} style={{width: '200px'}} alt="logo"/>
+              </Link>
+                <ul className="navbar-nav px-3 ml-auto">
+                    <li className="nav-item text-nowrap ">
+                        <Link to='' className="nav-link active" style={{fontWeight: 'bold', color: '#FFD000', marginRight: '40px'}}>
+                          <FaMedal size={28}/>
+                          <span style={{ color: '#000', marginRight: '10px' }}>INTEGRADOR OURO</span>
+                        </Link>
+                    </li>
+                    <li className="nav-item text-nowrap ">
+                        <Link to='' className="nav-link active" style={{fontWeight: 'bold', color: '#D3D3D3'}}>
+                          <FaRegQuestionCircle size={28}/>
+                        </Link>
+                    </li>
+                    <li className="nav-item text-nowrap ">
+                        <Link to='' className="nav-link active" style={{fontWeight: 'bold', color: '#D3D3D3'}}>
+                          <FaBell size={28}/>
+                        </Link>
+                    </li>
+                    <li className="nav-item text-nowrap ">
+                        <button onClick={() => this.showMenu()} className="nav-link active" style={{fontWeight: 'bold', color: '#00CA00', background: 'none', outline: 'none', border: 'none'}}>
+                          <FaRegUserCircle size={28}/>
+                        </button>
+
+                       {
+                          showMenu ? (
+                          <DropdownMenu className='shadow'>
+                            <ul>
+                              <li>
+                                <Link>
+                                 Meu Perfil
+                                </Link>
+                              </li>
+                              <li>
+                                <Link>
+                                  Meus Clientes
+                                </Link>
+                              </li>
+                              <li>
+                                <Link>
+                                  Meus Negócios
+                                </Link>
+                              </li>
+                              <li>
+                                <Link>
+                                  Meus Orçamentos
+                                </Link>
+                              </li>
+                              <li>
+                                <Link>
+                                  LogOut
+                                </Link>
+                              </li>
+                            </ul>
+                          </DropdownMenu>
+                         ) : (
+                           null
+                         )
+                       }
+
+                    </li>
+                </ul>
+          </Navbar> 
+      </div>
+    );
+  }
 }
--- siriuser/src/components/Header/styles.js
+++ siriuser/src/components/Header/styles.js
@@ -3,3 +3,41 @@ import styled from 'styled-components';
 export const Navbar = styled.nav`
   border-bottom: 1px solid #D3FAC1;
 `;
+
+export const DropdownMenu = styled.div`
+  position: absolute;
+  top: 80px;
+  right: 30px;
+  ul{
+    background-color: #fff;
+    height: 250px;
+    width: 200px;
+    border-radius: 4px;
+    list-style: none;
+    border: 1px solid #ddd;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+
+    li{
+      font-size: 20px;
+      padding: 8px 0;
+
+      & + li{
+        border-top: 1px solid #ddd;
+        
+      }
+      a{
+        text-decoration: none;
+        color: #D3D3D3;
+
+        &:hover{
+          color: #5d5d;
+        }
+      }
+    }
+  }
+
+
+
+`
