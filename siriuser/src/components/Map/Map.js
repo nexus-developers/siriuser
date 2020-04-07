d26b06618d2f1d3de245765274e14081250a6a3b
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2020-04-07 11:45:44 -0300

    Termino google maps

--- siriuser/src/components/Map/Map.js
+++ siriuser/src/components/Map/Map.js
@@ -1,16 +1,18 @@
-import React, { Component, useState } from 'react'
+import React, { Component } from 'react'
 
 import { Container, ContainerMap, InputMap, ButtonMap } from './styles'
 
 import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
 
-import PlacesAutocomplete ,{ geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
 class Maps extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            address: ''
+            address: '',
+            lat: -8.124176,
+            lng: -34.898745
         };
     }
 
@@ -20,11 +22,17 @@ class Maps extends Component{
         });
     }
 
-    handleSelect = async value => {};
+    handleSelect = async value => {
+        const result = await geocodeByAddress(value);
+        const latLng = await getLatLng(result[0]);
+        console.log(latLng)
+        this.setState(latLng);
+        this.setState({address: value});
+    };
 
     render(){
-        // const { address } = this.state
-        console.log(this.state.address)
+        // const { lat, lng } = this.state
+        // console.log(this.state.address)
         return(
             <Container>
                 <ContainerMap>
@@ -34,10 +42,12 @@ class Maps extends Component{
                     </ButtonMap> */}
                 </ContainerMap>
                 {/* Utilização google maps baseado em latitude e longitude */}
-                <GoogleMap defaultZoom={14} defaultCenter={{lat: -8.124176, lng: -34.898745}} style = {mapStyles} />
-                <PlacesAutocomplete value={this.state.address} onChange={this.address} onSelect={this.handleSelect()} ref='address'>
+                <GoogleMap defaultZoom={14} center={{lat: this.state.lat, lng: this.state.lng}} style = {mapStyles} />
+                <PlacesAutocomplete value={this.state.address} onChange={this.address} onSelect={this.handleSelect} ref='address'>
                     {({getInputProps, suggestions, getSuggestionItemProps, loading}) => (
                         <div>
+                            <p>Lat: {this.state.lat}</p>
+                            <p>Lng: {this.state.lng}</p>
                             <InputMap className="form-control shadow" {...getInputProps({placeholder: 'Digite o endereço'})} />
                             {/* <input {...getInputProps({ placeholder: "Type address" })} /> */}
                             
@@ -46,7 +56,11 @@ class Maps extends Component{
 
                                 {
                                     suggestions.map((suggestion => {
-                                        return <div>{suggestion.description}</div>
+                                        const style = {
+                                            backgroundColor: suggestion.active ? "#41b6e6" : "#fff"
+                                        };
+
+                                        return <div {...getSuggestionItemProps(suggestion, {style})}>{suggestion.description}</div>
                                     }))
                                 }
                             </div>
@@ -64,7 +78,7 @@ const WrappedMap = withScriptjs(withGoogleMap(Maps));
 export default WrappedMap;
 
 // export default GoogleApiWrapper({
-//     apiKey: ('AIzaSyAB-xvZm8wx8Doshepy284rjII_U2zZkfs')
+//     apiKey: ('AIzaSyAB-xvZm8wx8Doshepy284rjII_U2zZkfs') AIzaSyD7_bPvT5h2rMlVMADZwvPJoaydPsUBnJ8
 // })(Maps)
 
 const mapStyles = {
