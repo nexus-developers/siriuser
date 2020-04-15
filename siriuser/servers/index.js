68d1bc732ea7bd9ae00441fffa6462b12a269608
Geraldo Domingos <geraldo.dsn2000@gmail.com>
2020-04-15 16:16:41 -0300

    inicio da parte de alterações finais

--- siriuser/servers/index.js
+++ siriuser/servers/index.js
@@ -1,25 +1,2 @@
-const puppeteer = require('puppeteer')
-
-
-(async () => {
-    try{
-        const browser = await puppeteer.launch()
-        const page = await browser.newPage()
-
-        const htmlContent = `
-            <body>
-                <h1>Teste</h1>
-            </body>
-        `
-
-        await page.setContent(htmlContent)
-        await page.pdf({ path: 'mypdf.pdf', format: 'A4'})
-
-        console.log('done')
-
-        await browser.close()
-        
-    }catch(e){
-        console.log(e);
-    }
-})()
+const express = require('express')
+const bodyParser = require('')
